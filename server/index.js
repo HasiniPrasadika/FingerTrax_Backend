19e6a1b1290fdfa2ffce5adf6ce914aa3923fe34
HasiniPrasadika <gkhpmadhushani17@gmail.com>
2024-06-25 21:41:12 +0530

    fhgf

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ mongoose
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://fingertrax-seven.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
